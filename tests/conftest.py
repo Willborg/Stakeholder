6bd60fd247b438b5216e3f125849aa2d72057b0d
Willborg <131832494+Willborg@users.noreply.github.com>
2025-07-22 20:49:18 -0700

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-# tests/conftest.py
-import sys, pathlib
-
-# Add project root (one level up) to Python path so `import scripts...` works.
-ROOT = pathlib.Path(__file__).resolve().parents[1]
-sys.path.append(str(ROOT))
